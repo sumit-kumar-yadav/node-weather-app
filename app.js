8769d057ee534eab49cdbc66d9a41381fd880728
Sumit Yadav <sumityadav0376@gmail.com>
2023-02-08 18:40:57 +0530

    Get lat. & long. by place name using mapbox api

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const request = require('postman-request');
 const env = require('./private_constants');
+// encodeURIComponent( address )
 
-const weatherstackURL = `http://api.weatherstack.com/current?access_key=${env.WEATHERSTACK_ACCESS_KEY}&query=42.3605,-71.0596&units=f`
+const weatherstackURL = `http://api.weatherstack.com/current?access_key=${env.WEATHERSTACK_ACCESS_KEY}&query=42.3605,-71.0596&units=m`  // in degree celcius
 
 request({ url: weatherstackURL, json: true }, (error, response) => {
     if (error) {
@@ -13,3 +14,17 @@ request({ url: weatherstackURL, json: true }, (error, response) => {
     }
 })
 
+const geocodeURL = `https://api.mapbox.com/geocoding/v5/mapbox.places/philadelphia.json?access_token=${env.MAPBOX_ACCESS_KEY}&limit=1`  // Max limit is 10
+// philadelphia is the place name across which coordinates are to be extracted
+request({ url: geocodeURL, json: true }, (error, response) => {
+    debugger;
+    if (error) {
+        console.log('Unable to connect to location services!')
+    } else if (response.body.features.length === 0) {
+        console.log('Unable to find location. Try another search.')
+    } else {
+        const latitude = response.body.features[0].center[1]
+        const longitude = response.body.features[0].center[0]
+        console.log(latitude, longitude)
+    }
+})
