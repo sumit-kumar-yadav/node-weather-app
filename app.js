e1909ef9302bc5798e05f728472b4e70ad664278
Sumit Yadav <sumityadav0376@gmail.com>
2023-03-25 00:33:43 +0530

    Render html

--- app.js
+++ app.js
@@ -1,23 +1,57 @@
+const express = require('express');
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
-const address = process.argv[2]
+const app = express();
+const port = process.env.PORT || 8000;
 
-if (!address) {
-    console.log('Please provide an address')
-} else {
-    geocode(address, (error, { latitude, longitude, location } = {}) => {
-        if (error) {
-            return console.log(error)
-        }
+app.use(express.urlencoded({extended: false}));
 
-        forecast(latitude, longitude, (error, forecastData) => {
+// Accessing the static files like css, js 
+app.use(express.static('./assets'));
+
+// set up the view engine
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+// Root router
+app.get('/', function(req, res){
+    return res.render('index');
+});
+
+app.post('/get-weather', (req, res) => {
+    const address = req.body.area;
+
+    if (!address) {
+        return res.send('Please provide an address')
+    } else {
+        geocode(address, (error, { latitude, longitude, location } = {}) => {
             if (error) {
-                return console.log(error)
+                return res.send(error)
             }
+    
+            forecast(latitude, longitude, (error, forecastData) => {
+                if (error) {
+                    return res.send(error)
+                }
+    
+                return res.render('forecast', {
+                    location,
+                    forecastData
+                });
 
-            console.log(location)
-            console.log(forecastData)
+            })
         })
-    })
-}
+    }
+
+})
+
+
+
+app.listen(port, function(err){
+    if(err){
+        console.log(`Error in running the server : ${err}`);
+    }
+    console.log(`Server is running on port : ${port}`);
+});
+
