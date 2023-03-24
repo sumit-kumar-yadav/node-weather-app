23bc8ec0fd1bf60f5c4c9a3f76addf5c78871cb5
Sumit Yadav <sumityadav0376@gmail.com>
2023-03-24 23:33:44 +0530

    Handle undefined destructuring of objcts

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const address = process.argv[2]
 if (!address) {
     console.log('Please provide an address')
 } else {
-    geocode(address, (error, { latitude, longitude, location }) => {
+    geocode(address, (error, { latitude, longitude, location } = {}) => {
         if (error) {
             return console.log(error)
         }
